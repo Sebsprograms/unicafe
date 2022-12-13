168470a8712ece8baca50da25e2e823d88c4d50f
Sebsprograms <sebastianbenad72@gmail.com>
2022-12-12 21:38:42 -0700

    1.6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(prev => prev + 1)}>good</button>
+      <button onClick={() => setNeutral(prev => prev + 1)}>neutral</button>
+      <button onClick={() => setBad(prev => prev + 1)}>bad</button>
+      <h2>statistics</h2>
+      <h3>Good: {good}</h3>
+      <h3>Neutral: {neutral}</h3>
+      <h3>Bad: {bad}</h3>
     </div>
   );
 }
