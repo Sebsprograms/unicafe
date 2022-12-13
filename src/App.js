7952ec4c5a3074f274611a4835091fb3bca50a8d
Sebsprograms <sebastianbenad72@gmail.com>
2022-12-12 22:13:00 -0700

    1.10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { useState } from "react";
 
+function Statistic({ title, stat }) {
+  return (<h4>{title}: {stat.toFixed(2)}</h4>);
+}
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const average = total / 3;
+  const positive = (good / total) * 100;
+  const negative = (bad / total) * 100;
+
+  const statisticsElements = (<>
+        <h3>Good: {good}</h3>
+      <h3>Neutral: {neutral}</h3>
+      <h3>Bad: {bad}</h3>
+      <Statistic title={"Average"} stat={average} />
+      <Statistic title={"Positive"} stat={positive} />
+      <Statistic title={"Negative"} stat={negative} />
+  </>);
+
+  const statistics = total > 0 ? statisticsElements : <h3>No Feedback given</h3>
 
   return (
     <div className="App">
@@ -13,9 +32,7 @@ function App() {
       <button onClick={() => setNeutral(prev => prev + 1)}>neutral</button>
       <button onClick={() => setBad(prev => prev + 1)}>bad</button>
       <h2>statistics</h2>
-      <h3>Good: {good}</h3>
-      <h3>Neutral: {neutral}</h3>
-      <h3>Bad: {bad}</h3>
+      {statistics}
     </div>
   );
 }
